9f70dcfd7a5733481a95381f7fda6c9739df1300
geraldkachi <fitzgeraldkachi@gmail.com>
2021-04-14 04:34:30 +0100

    compose and graphql HOC in cart icon and more touching

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -6,8 +6,8 @@ import { Route } from 'react-router-dom';
 import { default as CollectionsOverview } from '../../components/collections-overview/CollectionsOverviewContainerGql';
 // this way or this way the container is still passed  dont want to change code so i used { default as  CollectionsOverview}
 // import CollectionsOverviewContainerGql from '../../components/collections-overview/CollectionsOverviewContainerGql';
-// import {default as CollectionPage} from '../collection/CollectionPageContainerGql';
-import CollectionPageContainerGql from '../collection/CollectionPageContainerGql';
+import {default as CollectionPage} from '../collection/CollectionPageContainerGql';
+// import CollectionPageContainerGql from '../collection/CollectionPageContainerGql';
 
 // import CollectionPage from '../collection/collection.component';
 // import {default as CollectionPage} from '../collection/CollectionPageContainerGql';
@@ -15,7 +15,7 @@ import CollectionPageContainerGql from '../collection/CollectionPageContainerGql
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} /> {/* CollectionsOverviewContainerGql */}
-    <Route path={`${match.path}/:collectionId`} component={CollectionPageContainerGql} /> {/* CollectionPageContainerGql */}
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} /> {/* CollectionPageContainerGql */}
   </div>  
 );
 
